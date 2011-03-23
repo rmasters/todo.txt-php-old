0e076f8e5c3e9546c49c997b3ecd37d91cdde9d2
Ross Masters <ross@rossmasters.com>
2011-03-23 22:45:06 +0000

    Added arrayaccess/iterator methods for task container

--- TodoTxt/TaskList.php
+++ TodoTxt/TaskList.php
@@ -8,13 +8,71 @@
 
 namespace TodoTxt;
 
+use \ArrayObject;
+use \Exception;
+
 /**
  * Contains tasks and performs operations to the whole list - e.g. sorting
  */
-class TaskList
+class TaskList implements \ArrayAccess, \Iterator
 {
     public static $sortIgnoreCase = true;
     public static $numericSort = false;
     public static $childSymbol = "_";
 
+    protected $tasks;
+    private $position;
+
+    public function __construct(array $lines = null) {
+        $this->tasks = new ArrayObject;
+
+        if (is_array($lines)) {
+            foreach ($lines as $line) {
+                $this->tasks->append(new Task($line));
+            }
+        }
+    }
+
+    public function offsetExists($index) {
+        return isset($this->tasks[$index]);
+    }
+
+    public function offsetGet($index) {
+        return $this->tasks[$index];
+    }
+
+    public function offsetSet($index, $task) {
+        if (!$task instanceof Task) {
+            $task = new Task($task);
+        }
+        $this->tasks[$index] = $tasks;
+    }
+
+    public function offsetUnset($index) {
+        unset($this->tasks[$index]);
+    }
+
+    public function current() {
+        return $this->tasks[$this->position];
+    }
+
+    public function key() {
+        return $this->position;
+    }
+
+    public function next() {
+        $this->position++;
+    }
+
+    public function rewind() {
+        $this->position = 1;
+    }
+
+    public function valid() {
+        return isset($this[$this->position]);
+    }
+
+    public function getArrayCopy() {
+        return $this->tasks->getArrayCopy();
+    }
 }
